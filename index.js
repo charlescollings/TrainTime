36d849bb525eb94c68f24abd9562e29fe8e3ca3a
Unknown <charlescollings@usa.net>
2018-04-05 22:28:25 -0500

    TrainTime first commit

--- index.js
+++ index.js
@@ -13,12 +13,12 @@
 var trainData = firebase.database();
 
 $('#add-train-btn').on('click', function() {
+    debugger;
     // grab user input
     let trainName = $('#train-name-input').val().trim();
     let destination = $('#destiniation-input').val().trim();
     let firstTrain = $('#first-train-input').val().trim();
     let frequency = $('#frequency-input').val().trim();
-console.log(trainName);
 
     // add user input to train object
     let newTrain = {
@@ -37,9 +37,33 @@ console.log(trainName);
 // add data event
 trainData.ref().on("child_added", function(childSnapshot, prevChildKey) {
     //store everything into a variable
-    // console.log(childSnapshot.val());
+    console.log(childSnapshot.val());
+    let tName = childSnapshot.val().name;
+    let tDestination = childSnapshot.val().destination;
+    let tFrequency = childSnapshot.val().frequency;
+    let tFirstTrain = childSnapshot.val().firstTrain;
+
+    let trainArr = tFirstTrain.split(':');
+    let trainTime = moment().hours(trainArr[0]).minutes(trainArr[1]);
+    let maxMoment = moment.max(moment(), trainTime);
+    let tMinutes;
+    let tArrival;
+
+    // if first train is later than current time, set arrival to the first train time
+    if(maxMoment === trainTime) {
+        tArrival = trainTime.format("hh:mm A");
+        tMinutes = trainTime.diff(moment(), "minutes");
+
+    } else {
+        // calculate minutes until arrival using math
+        let differenceTimes = moment().diff(trainTime, "minutes");
+        let tRemainder = differenceTimes % tFrequency;
+        tMinutes = tFirstTrain - tRemainder;
+        // calculate arrival time
+        tArrival = moment().add(tMinutes, "m").format("mm:hh A");
+    }
+
+    $("#train-table > tbody").append('<tr><td>' + tName + '</td><td>' + tDestination + '</td><td>' + tFrequency + '</td><td>' + tArrival + '</td><td>' + tMinutes + '</td></tr>');
 });
-
-
 
 
